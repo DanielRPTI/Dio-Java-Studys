693eb289df56d3adc459b74915e197bd04140930
DanielRPTI <drjunior988@hotmail.com>
2023-11-30 15:29:45 -0300

    Conhecendo metodos

--- projeto-inicial/src/MinhaClass.java
+++ projeto-inicial/src/MinhaClass.java
@@ -1,5 +1,14 @@
 public class MinhaClass {
   public static void main(String[] args) {
-    System.out.println("First content about anatomic class");
+    String primeiroNome = "Daniel";
+    String segundoNome =  "Junior";
+
+    String nomeCompleto =  nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+  }
+//Como criar o seu primeiro metodo , informar o tipo de retorno e o nome do metodo
+// e por fim os params a serem usados 
+  public static String nomeCompleto (String primeiroNome, String segundoNome) {
+   return primeiroNome.concat(" ").concat(segundoNome);
   }
 }
