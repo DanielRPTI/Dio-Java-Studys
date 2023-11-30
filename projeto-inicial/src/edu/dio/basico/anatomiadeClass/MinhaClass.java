d362c31ee983e6ee26bd4a56924d90564a67e095
DanielRPTI <drjunior988@hotmail.com>
2023-11-30 16:01:33 -0300

    Tipos de variaveis

--- projeto-inicial/src/edu/dio/basico/anatomiadeClass/MinhaClass.java
+++ projeto-inicial/src/edu/dio/basico/anatomiadeClass/MinhaClass.java
@@ -1,3 +1,4 @@
+package edu.dio.anatomiadeClass;
 public class MinhaClass {
   public static void main(String[] args) {
     String primeiroNome = "Daniel";
---
+++ projeto-inicial/src/edu/dio/basico/variaveis/TiposVariaveis.java
@@ -0,0 +1,5 @@
+package edu.dio.basico.variaveis;
+
+public class TiposVariaveis {
+  
+}
