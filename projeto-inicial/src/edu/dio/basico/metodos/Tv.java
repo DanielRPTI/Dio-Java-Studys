607368607455717fd99b475230502402455af888
DanielRPTI <drjunior988@hotmail.com>
2023-12-04 11:10:24 -0300

    Liga e desliga tv, aumenta volume e muda de canal

--- projeto-inicial/src/edu/dio/basico/metodos/Tv.java
+++ projeto-inicial/src/edu/dio/basico/metodos/Tv.java
@@ -5,6 +5,17 @@ public class Tv {
   int canal = 1;
   int volume = 25;
 
+  public void mudarCanal(int novoCanal) {
+    canal = novoCanal;
+  }
+
+  public void aumentarVolume () {
+    volume++;
+  }
+  public void diminuirVolume () {
+    volume--;
+  }
+
   public void ligar(){
     ligada = true;
   }
--- projeto-inicial/src/edu/dio/basico/metodos/Usuario.java
+++ projeto-inicial/src/edu/dio/basico/metodos/Usuario.java
@@ -7,8 +7,22 @@ public class Usuario {
     System.out.println("Canal atual? " + smartTv.canal);
     System.out.println("Volume atual? " + smartTv.volume);
 
+// liga e desliga :
     smartTv.ligar();
       System.out.println("Tv ligada? " + smartTv.ligada);
+    smartTv.desligar();
+      System.out.println("Tv ligada? " + smartTv.ligada);
 
+// aumenta e diminui volume 
+    smartTv.aumentarVolume();
+    System.out.println("Volume aumentado: " + smartTv.volume);
+
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    System.out.println("Volume atual" + smartTv.volume);
+//muda canal
+    System.out.println("Canal atual: " + smartTv.canal);
+    smartTv.mudarCanal(13);
+    System.out.println("Canal atual: " + smartTv.canal);
   }
 }
