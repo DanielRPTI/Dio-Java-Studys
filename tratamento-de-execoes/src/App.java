de1015c9bcb1a8cfd205053c7032a738ae4fb4d1
DanielRPTI <drjunior988@hotmail.com>
2024-01-05 15:42:25 -0300

    Trabalhando com try e catch

---
+++ tratamento-de-execoes/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        try {
+        System.out.println("Digite seu nome: ");
+        String nome = sc.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = sc.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = sc.nextDouble();
+
+        System.out.printf("Ola, me chamo %s %s e tenho %d minha altur é %.2f",nome, sobrenome, idade, altura );
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser númericos");
+        }
+
+    }
+}
