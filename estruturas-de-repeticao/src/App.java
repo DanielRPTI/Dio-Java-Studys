8e55dfd326909285d092c1ef51927533b7e3256d
DanielRPTI <drjunior988@hotmail.com>
2024-01-05 14:09:20 -0300

    estrutura do for

--- estruturas-de-repeticao/src/App.java
+++ estruturas-de-repeticao/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for(int carneiros = 0 ; carneiros <= 20 ; carneiros++ ){
+            System.out.println("Contador de carneirinhos: "+ carneiros);
+        }
+        System.out.println("Joaozinho dormiu!");
     }
 }
