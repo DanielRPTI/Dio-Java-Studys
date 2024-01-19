0e3455b295011dcdd954e612d3e68765b174babd
DanielRPTI <drjunior988@hotmail.com>
2024-01-19 14:11:38 -0300

    Test Menssenger sender

--- POO-Estudos/Poo/src/apps/MenssengerSender.java
+++ POO-Estudos/Poo/src/apps/MenssengerSender.java
@@ -5,7 +5,7 @@ public abstract class MenssengerSender  {
     public abstract void receberMensagem();
 
 //    public abstract  void salvarHistoricoMensagem();
-
+    //Apenas os filhos da classe MenssengerSender podem acessar a validação
     protected  void validarConexao(){
         System.out.println("Validando se está conectado a internet!");
     }
