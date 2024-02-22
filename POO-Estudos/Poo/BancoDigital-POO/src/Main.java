ae46afbd94489284baa450065d0a8b2f9d7272aa
DanielRPTI <drjunior988@hotmail.com>
2024-02-22 18:05:04 -0300

    Criando lista de contas , e criando o Cliente para cada conta

--- POO-Estudos/Poo/BancoDigital-POO/src/Main.java
+++ POO-Estudos/Poo/BancoDigital-POO/src/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
 	public static void main (String[] args) {
-		Conta contaCorrente = new ContaCorrente();
+		
+		Cliente daniel = new Cliente();
+		daniel.setNome("Daniel");
+		daniel.setIdade(21);
+		
+		Conta contaCorrente = new ContaCorrente(daniel);
 		contaCorrente.depositar(100);
-		Conta contaPoupanca = new ContaPoupanca();
+		Conta contaPoupanca = new ContaPoupanca(daniel);
 		contaPoupanca.depositar(500);
 		
 		contaCorrente.transferir(50, contaPoupanca);
