0ed181d51237798c76b6c5e6c542d3bf1fdfbace
DanielRPTI <drjunior988@hotmail.com>
2024-02-27 10:50:24 -0300

    Iniciando Estudo de estrutura de dados Fila(First in First out)

---
+++ EstruturaDeDados/src/one/digitalinovation/filas/No.java
@@ -0,0 +1,16 @@
+package one.digitalinovation.filas;
+
+public class No {
+	
+	private Object object;
+	private No refNo;
+	
+	public No() {
+	}
+	
+	public No(Object object) {
+		this.refNo = null;
+		this.object = object;
+	}
+	
+}
