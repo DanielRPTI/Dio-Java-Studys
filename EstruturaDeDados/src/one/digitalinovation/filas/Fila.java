4c474bcc57985c460ef8dd84e77b959a4110a583
DanielRPTI <drjunior988@hotmail.com>
2024-02-27 11:29:00 -0300

    Implementando o ultimo metodo ToString para darmos inicio aos testes da fila

--- EstruturaDeDados/src/one/digitalinovation/filas/Fila.java
+++ EstruturaDeDados/src/one/digitalinovation/filas/Fila.java
@@ -6,16 +6,70 @@ public class Fila {
 	public Fila() {
 		this.refNoEntradaFila = null;
 	}
-	
+
 	public void enqueue(No novoNo) {
 		novoNo.setRefNo(refNoEntradaFila);
 		refNoEntradaFila = novoNo;
 	}
-	
-	
-	
-	public boolean isEmpty() {
-		return refNoEntradaFila == null? true : false;
+
+	public No dequeue() {
+		if (!this.isEmpty()) {
+			No primeiroNo = refNoEntradaFila;
+			No noAuxiliar = refNoEntradaFila;
+			while (true) {
+				if (primeiroNo.getRefNo() != null) {
+					noAuxiliar = primeiroNo;
+					primeiroNo = primeiroNo.getRefNo();
+					;
+				} else {
+					noAuxiliar.setRefNo(null);
+					break;
+				}
+			}
+			return primeiroNo;
+		}
+		return null;
 	}
-	
+
+	public No first() {
+		if (!this.isEmpty()) {
+			No primeiroNo = refNoEntradaFila;
+			while (true) {
+				if (primeiroNo.getRefNo() != null) {
+					primeiroNo = primeiroNo.getRefNo();
+					;
+				} else {
+					break;
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return refNoEntradaFila == null ? true : false;
+	}
+
+	@Override
+	public String toString() {
+		String stringRetornoString = "";
+		No noAuxiliar = refNoEntradaFila;
+
+		if (refNoEntradaFila != null) {
+			while (true) {
+				stringRetornoString += "[No{objeto=" + noAuxiliar.getObject() + "}]-->";
+				if (noAuxiliar.getRefNo() != null) {
+					noAuxiliar = noAuxiliar.getRefNo();
+				} else {
+					stringRetornoString += "null";
+					break;
+				}
+			}
+		} else {
+			stringRetornoString = "null";
+		}
+
+		return stringRetornoString;
+	}
+
 }
