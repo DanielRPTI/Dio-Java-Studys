900f89e21704e960f6c30cecb9d24396039ec51e
DanielRPTI <drjunior988@hotmail.com>
2024-02-27 11:07:29 -0300

    Implementando os metodos enqueue e isEmpty ao projeto

--- EstruturaDeDados/src/one/digitalinovation/filas/No.java
+++ EstruturaDeDados/src/one/digitalinovation/filas/No.java
@@ -8,6 +8,27 @@ public class No {
 	public No() {
 	}
 	
+	@Override
+	public String toString() {
+		return "No [object=" + object + "]";
+	}
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public No getRefNo() {
+		return refNo;
+	}
+
+	public void setRefNo(No refNo) {
+		this.refNo = refNo;
+	}
+
 	public No(Object object) {
 		this.refNo = null;
 		this.object = object;
