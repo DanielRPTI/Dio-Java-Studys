00ef52507bb008d557f3d9eaaaf4f986615d3a07
DanielRPTI <drjunior988@hotmail.com>
2024-02-28 14:20:27 -0300

    Realizei a criação do metodo getNo e um validatedIndex, para verificar se o index é maior doque o informado

--- EstruturaDeDados/src/one/digitalinovation/listaencadeada/ListaEncadeada.java
+++ EstruturaDeDados/src/one/digitalinovation/listaencadeada/ListaEncadeada.java
@@ -10,19 +10,31 @@ public class ListaEncadeada<T> {
 
 	public void add(T contet) {
 		No<T> newNo = new No<>(contet);
-		if(this.isEmpty()) {
+		if (this.isEmpty()) {
 			refInput = newNo;
 			return;
 		}
-		
+
 		No<T> noAuxiliar = refInput;
-		for(int i = 0; i < this.size()-1; i++) {
+		for (int i = 0; i < this.size() - 1; i++) {
 			noAuxiliar = noAuxiliar.getNextNo();
 		}
 		noAuxiliar.setNextNo(newNo);
-		
+
 	}
-	
+
+	private No<T> getNo(int index) {
+
+		validatedIndex(index);
+		No<T> noAuxiliar = refInput;
+		No<T> noReturn = null;
+		for (int i = 0; i < this.size() - 1; i++) {
+			noReturn = noAuxiliar;
+			noAuxiliar = noAuxiliar.getNextNo();
+		}
+		return noReturn;
+	}
+
 	public int size() {
 		int sizeList = 0;
 		No<T> refAuxiliar = refInput;
@@ -40,6 +52,14 @@ public class ListaEncadeada<T> {
 			}
 		}
 		return sizeList;
+	}
+
+	private void validatedIndex(int index) {
+		if (index >= size()) {
+			int lastIndex = size() - 1;
+			throw new IndexOutOfBoundsException("Doesnt exist content in index" + index
+					+ "  this list. This list only go to index" + lastIndex + ".");
+		}
 	}
 
 	public boolean isEmpty() {
