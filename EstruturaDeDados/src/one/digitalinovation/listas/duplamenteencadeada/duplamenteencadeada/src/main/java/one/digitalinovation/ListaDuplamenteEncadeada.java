2c4f519675e83cd77859ab701696384a7d669b56
DanielRPTI <drjunior988@hotmail.com>
2024-02-28 19:41:43 -0300

    Adicionando os metodos add e add(Index)

--- EstruturaDeDados/src/one/digitalinovation/listas/duplamenteencadeada/duplamenteencadeada/src/main/java/one/digitalinovation/ListaDuplamenteEncadeada.java
+++ EstruturaDeDados/src/one/digitalinovation/listas/duplamenteencadeada/duplamenteencadeada/src/main/java/one/digitalinovation/ListaDuplamenteEncadeada.java
@@ -15,6 +15,42 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    //metodo add padrao adicionando sempre o elemento ao ultimo local da lista
+    public void add(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista ++;
+    }
+
+    //metodo add(index) podendo te oferecer a possibilidade de adicionar o elemento onde for informado o index do local
+    public void add(T elemento, int index){
+        NoDuplo<T> noAuxiliar = getNo(index);
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(noAuxiliar);
+        if(novoNo.getNoProximo() != null){
+            novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        }else{
+            novoNo.setNoPrevio(ultimoNo);
+            ultimoNo = novoNo;
+        }
+        if (index == 0){
+            primeiroNo = novoNo;
+        }else{
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
 
@@ -26,6 +62,6 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size(){
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 }
