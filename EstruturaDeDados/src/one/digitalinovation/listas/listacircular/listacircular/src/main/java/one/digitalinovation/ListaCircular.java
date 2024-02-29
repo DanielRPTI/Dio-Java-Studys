2aaf1317c8f728c13063f859a4e9902351525f1c
DanielRPTI <drjunior988@hotmail.com>
2024-02-29 11:24:41 -0300

    Implementando|Documentando o metodo remove de nossa lista circular

--- EstruturaDeDados/src/one/digitalinovation/listas/listacircular/listacircular/src/main/java/one/digitalinovation/ListaCircular.java
+++ EstruturaDeDados/src/one/digitalinovation/listas/listacircular/listacircular/src/main/java/one/digitalinovation/ListaCircular.java
@@ -27,9 +27,31 @@ public class ListaCircular<T> {
         return this.getNo(index).getConteudo();
     }
 
-
-
-
+    //Metodo Remove()
+    /*
+    * Primeiro verificamos se o index é maior que o tamanho da lista , se for ele lança uma exception
+    * iniciamos o nosso noAuxiliar recebendo nossa cauda
+    * após isso verificamos se o nosso index é == a 0 || 1 e realizamos a logica de ambas condições
+    * caso seja um indice mais ao meio passamos por um laço for onde o seguimos a logica do index-1 ele pegara o index informado e subtraira
+    * e irá começar em um nó antes do informado e assim ele percorre a cauda até o no a frente do removido
+    */
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior doque o tamanho da lista!");
+        No<T> noAuxiliar = this.cauda;
+        if(index == 0){
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        }else if (index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else{
+            for (int i = 0; i < index-1; i++){
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true : false;
     }
