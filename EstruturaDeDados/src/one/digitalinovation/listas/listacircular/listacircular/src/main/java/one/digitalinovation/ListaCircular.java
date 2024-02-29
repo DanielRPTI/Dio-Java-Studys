7da5125571fead1ea340e451f0e1d209e330539f
DanielRPTI <drjunior988@hotmail.com>
2024-02-29 10:29:45 -0300

    Criandos os metodos getNo e get de nossa lista circular

--- EstruturaDeDados/src/one/digitalinovation/listas/listacircular/listacircular/src/main/java/one/digitalinovation/ListaCircular.java
+++ EstruturaDeDados/src/one/digitalinovation/listas/listacircular/listacircular/src/main/java/one/digitalinovation/ListaCircular.java
@@ -1,10 +1,34 @@
 package one.digitalinovation;
 
 public class ListaCircular<T> {
-
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    /*
+    * Neste get no de lista circular verificamos primeiramente se nossa lista está vazia
+    * depois verificamos se nosso index informado é 0 se for ele retorna o no da cauda
+    * após isso se passarmos das verificações iniciamos nosso for com nosso noAuxiliar para percorrer a lista
+    */
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+        if(index == 0){
+            return this.cauda;
+        }
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0;  (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+    //Em nosso metodo get é simples , apenas utilizamos nosso generic e retornamos nosso getNo com o index desejado que é retornado em nosso getConteudo
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+
+
 
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true : false;
@@ -13,5 +37,6 @@ public class ListaCircular<T> {
     public int size(){
         return this.tamanhoLista;
     }
+
 
 }
