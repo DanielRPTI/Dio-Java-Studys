94b121697012b45e85407fb59ed6933937789cfc
DanielRPTI <drjunior988@hotmail.com>
2024-03-08 23:57:47 -0300

    Inciando Desafios da aula

---
+++ CollectionsaJava/collections/src/main/java/com/dio/collection/map/DesafioEstadosNE.java
@@ -0,0 +1,12 @@
+package com.dio.collection.map;
+
+import java.util.*;
+
+public class DesafioEstadosNE {
+    public static void main(String[] args) {
+        Map<String, Integer> estadosNe = new HashMap<>(){{
+            put("PE", 9616621);
+        }};
+
+    }
+}
